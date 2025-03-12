a556b541cdf8a6f03b038306929a8a25480caa84
Akila Thikshana <akii.ac.rjt@gmail.com>
2025-03-13 00:02:55 +0530

    Update Loop 9.c

--- Loop 9.c
+++ Loop 9.c
@@ -19,15 +19,15 @@ int main() {
        scanf("%d", &size);
     
         for (i = 0; i < size; i++) {       
-        for (j = 0; j < size; j++) {    
-            if ((i + j) % 2 == 0) {    
-                printf("1 ");
-            } else {
-                printf("0 ");
+            for (j = 0; j < size; j++) {    
+                if ((i + j) % 2 == 0) {    
+                    printf("1 ");
+                } else {
+                    printf("0 ");
+                }
             }
+            printf("\n");
         }
-        printf("\n");
-    }
     
     return 0;
 }
